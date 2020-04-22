474c521cd25e71dde9da7ec41695ddf57ca5103c
muxianliangqin <muxianliangqin@users.noreply.github.com>
2020-04-22 16:16:41 +0800

    网页爬虫的python实现部分

---
+++ crawler/index.py
@@ -0,0 +1,413 @@
+import pymysql
+import schedule, time, os, sys, traceback, re, threading
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from flask import Flask, request, jsonify
+
+# crawler的父目录加入系统搜索目录，以使得在linux系统中可以import自定义的模块
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from crawler.utils import base as base_util
+from crawler.config.index import config
+
+
+class Crawler():
+    _instance_lock = threading.Lock()
+
+    browser = None
+    conn = None
+    cursor = None
+
+    def __init__(self):
+        pass
+
+    # 实现单例模式
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(Crawler, "_instance"):
+            with Crawler._instance_lock:
+                if not hasattr(Crawler, "_instance"):
+                    Crawler._instance = object.__new__(cls)
+        return Crawler._instance
+
+    def get_conn(self):
+        try:
+            self.conn.ping(reconnect=True)
+        except Exception as e:
+            self.conn = pymysql.connect(host=config.MYSQL['host'], port=config.MYSQL['port'],
+                                   user=config.MYSQL['user'], passwd=config.MYSQL['password'],
+                                   db=config.MYSQL['db'], charset=config.MYSQL['charset'])
+        finally:
+            self.cursor = self.conn.cursor()
+
+
+    def get_browser(self):
+        if self.browser is None:
+            chrome_options = Options()
+            chrome_options.add_argument('--headless')
+            chrome_options.add_argument('--no-sandbox')
+            chrome_options.add_argument('--disable-dev-shm-usage')
+            chrome_options.add_argument('blink-settings=imagesEnabled=false')
+            chrome_options.add_argument('--disable-gpu')
+            # chrome_options.add_experimental_option("prefs", {
+            #     "download.default_directory": r"D:\\workspace",
+            #     "download.prompt_for_download": False,
+            #     "download.directory_upgrade": True,
+            #     "safebrowsing.enabled": True
+            # })
+            chrome_options.add_experimental_option("prefs", {
+                'profile.default_content_settings.popups': 0,
+                'download.default_directory': 'D:\\workspace'
+            })
+            self.browser = webdriver.Chrome(options=chrome_options)
+
+
+    # 申明 browser和conn变量
+    def init(self):
+        self.get_browser()
+        self.get_conn()
+
+    def get_categories(self, category_id=None, limit=100):
+        category_sql = '''
+        select id, url, xpath_title, charset 
+        from category 
+        where (%s is null and category_state = 0) 
+        or (%s is not null and id = %s)
+        limit %s
+        '''
+        self.cursor.execute(category_sql, (category_id, category_id, category_id, limit))
+        categories = self.cursor.fetchall()
+        t4 = time.time()
+        return categories
+
+    def crawl_title(self, categories):
+        new_insert_sql = '''
+        insert into news (url, title, category_id) 
+        select %s, %s, %s from dual 
+        where not exists (select 1 from news n where n.url = %s)
+        '''
+        title_results = {}
+        for category in categories:
+            category_id, url, xpath, charset = category
+            print('\n时间：{}，新闻分类：{}'.format(base_util.cur_time(), url))
+            title_result = {
+                'category_id': category_id,
+                'url': url,
+                'xpath': xpath,
+                'charset': charset,
+                'news': None,
+                'error': None
+            }
+            try:
+                self.browser.get(url)
+                xpath_res = self.browser.find_element_by_xpath(xpath)
+                news = xpath_res.find_elements_by_tag_name('a')
+                invalid_title = r'^(首页|下一页|上一页|确定|末页|尾页|更多(>>)?|\d+)$'
+                invalid_href = r'^(\.\/|\.\.\/)((\.\.\/)+)?$'
+                sql_input = []
+                news_results = []
+                for n in news:
+                    href = n.get_attribute('href')
+                    title = n.get_attribute('title')
+                    if not title:
+                        title = n.text
+                    if re.match(invalid_title, title) or re.match(invalid_href, href):
+                        continue
+                    href = base_util.url_format(url, href)[2]
+                    sql_input.append((href, title, category_id, href))
+                    news_results.append({
+                        'url': href,
+                        'title': title
+                    })
+                if len(sql_input) > 0:
+                    # print('抓取的结果：', sql_input)
+                    self.cursor.executemany(new_insert_sql, sql_input)
+                    self.conn.commit()
+                title_result['news'] = news_results
+            except Exception as e:
+                print('爬取标题:{}出错，error: {}'.format(url, traceback.print_exc()))
+                title_result['error'] = repr(e)
+                self.conn.rollback()
+            title_results[url] = title_result
+        return title_results
+
+    def get_news(self, category_id=None, limit=100):
+        news_sql = '''
+            select n.id, n.url, n.category_id, c.xpath_text, c.charset 
+            from news n 
+            join category c on n.category_id = c.id 
+            where (%s is null and (n.news_state = 0 and (n.text_state = '0' or n.attachment_state = '0')))
+            or (%s is not null and c.id = %s)
+            and c.xpath_text is not null
+            limit %s
+            '''
+        self.cursor.execute(news_sql, (category_id, category_id, category_id, limit))
+        news = self.cursor.fetchall()
+        return news
+
+    def get_news_by_url(self, url):
+        news_sql = '''
+            select n.id, n.url, n.category_id, c.xpath_text, c.charset 
+            from news n 
+            join category c on c.id = n.category_id
+            where n.url = %s
+            '''
+        self.cursor.execute(news_sql, url)
+        news = self.cursor.fetchall()
+        return news
+
+    def text_func(self, new, html):
+        text_sql = '''
+        replace into texts (news_id, text, keyword_result) values (%s, %s, %s)
+        '''
+        news_update_text_sql = '''
+        update news set text_state = '1' where id = %s
+        '''
+        keyword_sql = '''
+            select k.reg_exp 
+            from keyword k 
+            join web_keyword wk on wk.keyword_id = k.id 
+            where wk.category_id = %s
+        '''
+        news_id, url, category_id, xpath, charset = new[0], new[1], new[2], new[3], new[4]
+        text = None
+        text_error = None
+        try:
+            text = html.text.strip()
+            # print('text:{}'.format(text))
+            # 关键字暂时不获取，拟改为标签设置
+            # c_ks = dict()
+            # if category_id not in c_ks:
+            #     cursor.execute(keyword_sql, category_id)
+            #     keywords = cursor.fetchall()
+            #     c_ks[category_id] = [k[0] for k in keywords]
+            # 保存新闻正文
+            # regexps = c_ks[category_id]
+            # results = []
+            # for regexp in regexps:
+            #     regexp = regexp[1:]
+            #     idx = regexp.rfind('/')
+            #     # modifier = regexp[idx+1:]
+            #     # '/\d+(\.\d+)?[亿|万](美)?元/g'  '\\d+(\\.\\d+)?[亿|万](美)?元'
+            #     regexp = regexp[:idx]
+            #     res = re.findall('({})'.format(regexp), text)
+            #     res = [i[0] for i in res]
+            #     results.extend(res)
+            # print('results:{}'.format(results))
+            # keyword_result = ','.join(results)
+            keyword_result = ''
+            # 保存正文内容
+            self.cursor.execute(text_sql, (news_id, text, keyword_result))
+            # 改变正文已抓取状态
+            self.cursor.execute(news_update_text_sql, news_id)
+            self.conn.commit()
+        except Exception as e:
+            print('爬取正文:{}出错，error: {}'.format(url, traceback.print_exc()))
+            text_error = repr(e)
+            self.conn.rollback()
+        return [text, text_error]
+
+
+    def attachment_func(self, new, html):
+        news_update_attachment_sql = '''
+        update news set attachment_state = %s where id = %s
+        '''
+        attachments_sql = '''
+        insert into attachments (news_id, path, name, format) values (%s, %s, %s, %s)
+        '''
+        news_id, url, category_id, xpath, charset = new[0], new[1], new[2], new[3], new[4]
+        attachment_results = []
+        attachments_error = None
+        try:
+            # 保存新闻附件
+            attachments = []
+            links = html.find_elements_by_tag_name('a')
+            for link in links:
+                href = link.get_attribute('href')
+                if re.match('.*\.(xls|xlsx|doc|docx|pdf)', str.lower(href)):
+                    attachments.append(base_util.attachment_format(link, url, href))
+            videos = html.find_elements_by_tag_name('video')
+            for video in videos:
+                src = video.get_attribute('src')
+                if re.match('.*\.(webm|mp4|ogg)', str.lower(src)):
+                    attachments.append(base_util.attachment_format(video, url, src))
+            images = html.find_elements_by_tag_name('img')
+            for img in images:
+                src = img.get_attribute('src')
+                if re.match('.*\.(jpg|jpeg|gif|png|bmp)', str.lower(src)):
+                    attachments.append(base_util.attachment_format(img, url, src))
+            # 附件下载状态， 0-未下载，1-已下载或忽略下载
+            attachment_state = '0'
+            for attachment in attachments:
+                file_url, file_format, file_name = attachment
+                # print('附件下载，路径：{}, 格式：{}， 名称：{}'.format(file_url, file_format, file_name))
+                attachment_result = {
+                    'url': file_url,
+                    'format': file_format,
+                    'name': file_name,
+                    'download_flag': None,
+                    'too_small_flag': None,
+                }
+                attachment_dir = config.ATTACHMENT_DIR
+                # 确定保存附件的文件夹
+                file_dir = time.strftime('%Y/%m/%d')
+                attachment_dir = os.path.join(attachment_dir, file_dir)
+                if not os.path.exists(attachment_dir):
+                    os.makedirs(attachment_dir)
+                attachment_name = time.strftime('%H%M%S') + '_' + base_util.random_str()
+                attachment_path = attachment_dir + os.path.sep + attachment_name + '.' + file_format
+                attachment_path = base_util.url_linux(attachment_path)
+                # 50k以下的图片，不下载
+                download_flag, too_small_flag = base_util.download_file(file_url, attachment_path, 50 * 1024)
+                if download_flag:
+                    attachment_state = '1'
+                    self.cursor.execute(attachments_sql, (news_id, attachment_path, file_name, file_format))
+                attachment_result['download_flag'] = download_flag
+                attachment_result['too_small_flag'] = too_small_flag
+                attachment_results.append(attachment_result)
+            # 修改附件下载状态
+            self.cursor.execute(news_update_attachment_sql, (attachment_state, news_id))
+            # 每条新闻的正文、附件等数据保存合并提交
+            self.conn.commit()
+        except Exception as e:
+            print('下载附件:{}出错，error: {}'.format(url, traceback.print_exc()))
+            attachments_error = repr(e)
+            self.conn.rollback()
+        return [attachment_results, attachments_error]
+
+    def crawl_text(self, news):
+        text_results = {}
+        for new in news:
+            news_id, url, category_id, xpath, charset = new[0], new[1], new[2], new[3], new[4]
+            print('抓取正文和附件，url:{}, xpath:{}, category_id:{}, charset:{}'.format(url, xpath, category_id, charset))
+            text_result = {
+                'news_id': news_id,
+                'url': url,
+                'category_id': category_id,
+                'xpath': xpath,
+                'charset': charset,
+                'text': None,
+                'attachments': None,
+                'text_error': None,
+                'attachments_error': None
+            }
+            self.browser.get(url)
+            html = self.browser.find_element_by_xpath(xpath)
+            text_thread = ResultThread(self.text_func, (new, html))
+            attachment_thread = ResultThread(self.attachment_func, (new, html))
+            text_thread.start()
+            attachment_thread.start()
+            # 等待子线程结束后，再往后面执行
+            text_thread.join()
+            attachment_thread.join()
+            text_result['text'], text_result['text_error'] = text_thread.get_result()
+            text_result['attachments'], text_result['attachments_error'] = attachment_thread.get_result()
+            text_results[url] = text_result
+        return text_results
+
+    def crawler(self):
+        try:
+            # 初始化conn、cursor和browser
+            self.init()
+            # 爬取标题数据
+            self.crawl_title(self.get_categories())
+            # 爬取正文数据
+            self.crawl_text(self.get_news())
+            print('批次结束')
+        except Warning as w:
+            print('warning: {}'.format(w))
+        except Exception as e:
+            print('error: {}'.format(traceback.print_exc()))
+        # finally:
+        # ResultThread(close_conn).start()
+        # ResultThread(close_browser).start()
+
+    def test(self, request_id):
+        result = {
+            'errorCode': '0001',
+            'errorMsg': '抓取标题失败',
+            'data': None
+        }
+        try:
+            self.init()
+            title_results = self.crawl_title(self.get_categories(request_id))
+            # 如果爬取的结果不为空，取其中一条
+            for key, value in title_results.items():
+                news = value.get('news')
+                # 如果标题中新闻页列表不为空，取第一条
+                result['errorCode'] = '0002'
+                result['errorMsg'] = '抓取正文或附件失败'
+                for i, new in enumerate(news):
+                    url = new.get('url')
+                    text_results = self.crawl_text(self.get_news_by_url(url))
+                    new.update(text_results.get(url))
+                    news[i] = new
+                    result['errorCode'] = '0000'
+                    result['errorMsg'] = '抓取成功'
+                    # 成功获取一条新闻的正文和附件后退出循环
+                    break
+                value['news'] = news
+                result['data'] = value
+                break
+        except Warning as w:
+            print('warning: {}'.format(w))
+        except Exception as e:
+            print('error: {}'.format(traceback.print_exc()))
+            result['errorCode'] = '9999'
+            result['errorMsg'] = repr(e)
+        # finally:
+            # ResultThread(close_conn).start()
+            # ResultThread(close_browser).start()
+        return result
+
+    def close_conn(self):
+        if self.conn is not None:
+            self.conn.close()
+
+    def close_browser(self):
+        if self.browser is not None:
+            self.browser.close()
+
+class ResultThread(threading.Thread):
+    def __init__(self, func, args=()):
+        super(ResultThread, self).__init__()
+        self.func = func
+        self.args = args
+        self.result = None
+
+    def run(self):
+        self.result = self.func(*self.args)
+
+    def get_result(self):
+        try:
+            return self.result
+        except:
+            return None
+
+class CrawlThread (threading.Thread):
+    def __init__(self, crawler):
+        threading.Thread.__init__(self)
+        self.crawler = crawler
+
+    def run(self):
+        if config.ENV == 'prod':
+            self.crawler.crawler()
+            schedule.every(30).minutes.do(self.crawler.crawler)
+            while True:
+                schedule.run_pending()
+
+# 创建一个实例
+crawler_instance = Crawler()
+CrawlThread(crawler_instance).start()
+
+# 测试网站爬取的参数是否设置正确
+app = Flask(__name__)
+
+@app.route("/crawler/test", methods=['POST'])
+def test():
+    request_id = request.form['id']
+    result = crawler_instance.test(request_id)
+    # 返回json数据
+    return jsonify(result)
+
+app.run(host=config.FLASK['host'], port=config.FLASK['port'])
+
+
---
+++ crawler/utils/base.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random, re, requests, os, time
+from urllib.parse import urlparse, urlunparse
+
+def url_format(url_absolute, url):
+    """
+    此方法用于格式化url路径
+    :param url_absolute:绝对路径
+    :param url:需要格式化的路径
+    :return : dict
+    list = [url_format_bool:是否格式化成功,url_format_absolute:url格式化成绝对路径，url_format_relative:url格式化成最简相对路径]
+    """
+    # 格式化结果标记
+    # print('url_absolute=%s,url=%s' % (url_absolute,url))
+    format_bool = True
+    url_abs_parse = urlparse(url_absolute)
+    if not url_abs_parse.netloc:
+        return [False, False, url_absolute, url]
+    url_parse = urlparse(url)
+    url_abs_levels = url_abs_parse.path.split('/')
+    url_levels = url_parse.path.split('/')
+    url_abs_copy = url_abs_levels.copy()
+    url_levels_copy1 = url_levels.copy()
+    url_levels_copy2 = url_levels.copy()
+    # url 为绝对路径
+    if url_parse.netloc:
+        # 域名不同，格式化失败
+        if url_parse.netloc != url_abs_parse.netloc:
+            return [True, True, url, url]
+        # 从左至右判断是否存在相同级别名称，如有则删除
+        for i, url_abs in enumerate(url_abs_levels):
+            if len(url_levels) > i:
+                url_rel = url_levels[i]
+                if url_abs == url_rel:
+                    url_abs_copy.remove(url_abs)
+                    url_levels_copy2.remove(url_rel)
+                else:
+                    break
+            else:
+                break
+        # 根据绝对路径剩余的层级数，添加相应的相对符号
+        for i in range(len(url_abs_copy)):
+            if i == 0:
+                url_levels_copy2.insert(0, '.')
+            elif i == 1:
+                url_levels_copy2[0] = '..'
+            else:
+                url_levels_copy2.insert(0, '..')
+        return url_format(url_absolute, '/'.join(url_levels_copy2))
+    else:
+        for i, url_level in enumerate(url_levels):
+            # url相对根目录
+            if url_level == '':
+                if len(url_levels) == 1:
+                    url_levels_copy1.clear()
+                    url_levels_copy1.clear()
+                else:
+                    if url_level != url_levels[-1]:
+                        url_abs_copy.clear()
+                        url_levels_copy1 = url_levels[1:]
+                        url_levels_copy2 = url_levels[1:]
+                # 根据绝对路径剩余的层级数，添加相应的相对符号
+                url_abs_copy2 = [i for i in url_abs_copy if i not in ['', '.', '..']]
+                for i in range(len(url_abs_copy2)):
+                    if i == 0:
+                        url_levels_copy2.insert(0, '.')
+                    elif i == 1:
+                        url_levels_copy2[0] = '..'
+                    else:
+                        url_levels_copy2.insert(0, '..')
+                break
+            # url同级
+            elif url_level == '.':
+                if url_abs_copy:
+                    url_abs_copy.pop()
+                url_levels_copy1.remove(url_level)
+            # url往上一级
+            elif url_level == '..':
+                if url_abs_copy:
+                    url_abs_copy.pop()
+                url_levels_copy1.remove(url_level)
+                if i == 0:
+                    if url_abs_copy:
+                        url_abs_copy.pop()
+            else:
+                # url无层级符号，也就是同级
+                if url_levels[0] not in ['', '.', '..']:
+                    if url_abs_copy:
+                        url_abs_copy.pop()
+                # url和url_absolute存在层级重复
+                elif url_level in url_abs_copy and url_level != url_levels[-1]:
+                    url_abs_copy.remove(url_level)
+                    url_levels_copy2.remove(url_level)
+                else:
+                    pass
+
+        formatted_absolute = urlunparse([url_abs_parse.scheme, url_abs_parse.netloc,
+                                         '/'.join(url_abs_copy + url_levels_copy1), '', '', ''])
+        formatted_relative = urlunparse(['', '', '/'.join(url_levels_copy2), '', '', ''])
+    formatted = [format_bool, False, formatted_absolute, formatted_relative]
+    return formatted
+
+
+def random_str():
+    rand_str =  ''.join([str(random.choice(range(10))) for i in range(10)])
+    return rand_str
+
+
+def url_linux(url):
+    url = re.sub(r'\\+', '/', url)
+    url = re.sub(r'/+', '/', url)
+    return url
+
+def attachment_format(ele, sup_url, url):
+    '''
+    :param ele: 附件所在的js节点
+    :param sup_url: 附件所在的页面的url
+    :param url: 附件的url
+    :return:
+    '''
+    # 附加的绝对路径
+    file_url = url_format(sup_url, url)[2]
+    # 附件格式
+    file_format = file_url[str.rindex(file_url, '.') + 1:]
+    # 附件原名称
+    if ele.text:
+        file_name = ele.text.strip()
+    else:
+        url_temp = file_url[:str.rindex(file_url, '.')]
+        file_name = url_temp[str.rindex(url_temp, '/') + 1:]
+    return [file_url, file_format, file_name]
+
+
+def download_file(url, path, min_size, try_time=0):
+    '''
+    下载文件
+    :param url:
+    :param path:
+    :param min_size 文件最小尺寸
+    :param try_time 尝试下载次数
+    :return: [是否下载成功, 是否文件太小]
+    '''
+    download_flag = False
+    too_small_flag = False
+    if try_time > 0:
+        print('尝试下载次数:{}'.format(try_time))
+    max_try_time = 5
+    if try_time >= max_try_time:
+        os.remove(path)
+        return [False, False]
+    start_index = 0
+    if os.path.exists(path):
+        start_index = os.path.getsize(path)
+    headers = {
+        'Range': 'bytes:{}-'.format(start_index)
+    }
+    with requests.get(url, headers, stream=True) as response:
+        http_size = int(response.headers['Content-Length'])
+        # 如果是图片，并且大小小于min_size，则不下载
+        if re.match('.*\.(jpg|jpeg|gif|png|bmp)', str.lower(url)) and http_size < min_size:
+            too_small_flag = True
+        else:
+            with open(path, 'wb') as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+            local_size = os.path.getsize(path)
+            # 如果http返回的文件大小和下载下来的文件大小不一致，重新下载文件
+            if http_size != local_size:
+                try_time += 1
+                return download_file(url, path, min_size, try_time)
+            else:
+                download_flag = True
+    return [download_flag, too_small_flag]
+
+
+def cur_time():
+    return time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+
+
+def str_time(ti):
+    hour = int(ti) // (60 * 60)
+    ti -= hour * (60 * 60)
+    minute = int(ti) // 60
+    ti -= minute * 60
+    second = int(ti)
+    ti -= second
+    millisecond = int(ti * 1000)
+    return '{}小时 {}分钟 {}秒 {}毫秒'.format(hour,minute,second,millisecond)
